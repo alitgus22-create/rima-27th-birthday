834354853e0abb39b91c8df32a8a634c9405d162
alitgus22-create <alitgus22@gmail.com>
2026-09-17 21:24:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-// Musik sengaja belum dipasang.
-// Nanti cukup tambahkan file musik (mis. music.mp3) dan audio element
-// jika musik sudah dipilih.
+// Musik sengaja belum dipasang. Tambahkan nanti jika file musik sudah dipilih.
